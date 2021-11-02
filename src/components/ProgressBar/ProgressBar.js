cc1eb6594df3e3378c21f5a83eedf70cc6dc4b33
natuShubham96 <natushubham96@gmail.com>
2021-11-02 16:39:46 +0530

    Progress Bar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,50 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let Component = null;
+  switch(size) {
+    case 'small' : Component = SmallWrapper;
+    break;
+    case 'medium' : Component = MediumWrapper;
+    break;
+    case 'large' : Component = LargeWrapper;
+    break;
+  }
+  return <Component>
+    <Indicator value={value} />
+  </Component>;
 };
+
+const Wrapper = styled.div`
+width: 370px;
+background-color: ${COLORS.transparentGray15};
+border-radius: 4px;
+box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+overflow: hidden;
+`
+
+const SmallWrapper = styled(Wrapper)`
+height: 8px;
+`
+
+const MediumWrapper = styled(Wrapper)`
+height: 12px;
+`
+
+const LargeWrapper = styled(Wrapper)`
+height: 24px;
+border-radius: 8px;
+padding-left: 4px;
+padding-right: 4px;
+`
+
+const Indicator = styled.div`
+height: inherit;
+width: ${props => props.value + '%'};
+background-color: ${COLORS.primary};
+border-radius: inherit;
+border-top-right-radius: ${props => props.value===100? 'inherit' : 0};
+border-bottom-right-radius: ${props => props.value===100? 'inherit' : 0};
+`
 
 export default ProgressBar;
